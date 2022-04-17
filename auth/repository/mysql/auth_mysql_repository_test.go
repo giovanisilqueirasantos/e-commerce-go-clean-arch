169f3e2b16b43af2e5feffd4389864c2e7c9f6ee
giovanisilqueirasantos <giovani.silqueira.santos@gmail.com>
2022-04-17 10:48:12 -0300

    add test to storeAuthWithUser in auth repository

--- auth/repository/mysql/auth_mysql_repository_test.go
+++ auth/repository/mysql/auth_mysql_repository_test.go
@@ -3,9 +3,11 @@ package mysql
 import (
 	"context"
 	"errors"
+	"regexp"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/giovanisilqueirasantos/e-commerce-go-clean-arch/domain"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -28,6 +30,10 @@ func TestGetByLoginNotFound(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Nil(t, auth)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
 }
 
 func TestGetByLoginError(t *testing.T) {
@@ -46,6 +52,10 @@ func TestGetByLoginError(t *testing.T) {
 	_, err = authMysqlRepository.GetByLogin(context.Background(), "login")
 
 	assert.Error(t, err)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
 }
 
 func TestGetByLogin(t *testing.T) {
@@ -69,4 +79,89 @@ func TestGetByLogin(t *testing.T) {
 	assert.Equal(t, int64(1), auth.ID)
 	assert.Equal(t, "login", auth.Login)
 	assert.Equal(t, "password", auth.Password)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestStoreWithUserStoreUserError(t *testing.T) {
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		t.Fatalf("error when opening a stub database conn %s", err)
+	}
+
+	query := regexp.QuoteMeta("INSERT INTO users (email, first_name, last_name, phone_number, address_city, address_state, address_neighborhood, address_street, address_number, address_zipcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);")
+
+	mock.ExpectBegin()
+	mock.ExpectPrepare(query)
+	mock.ExpectExec(query).WithArgs("", "", "", "", "", "", "", "", "", "").WillReturnError(errors.New("error message"))
+	mock.ExpectRollback()
+
+	authMysqlRepository := NewAuthMysqlRepository(db)
+
+	err = authMysqlRepository.StoreWithUser(context.Background(), &domain.Auth{}, &domain.User{})
+
+	assert.Error(t, err)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestStoreWithUserStoreAuthError(t *testing.T) {
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		t.Fatalf("error when opening a stub database conn %s", err)
+	}
+
+	storeUserQuery := regexp.QuoteMeta("INSERT INTO users (email, first_name, last_name, phone_number, address_city, address_state, address_neighborhood, address_street, address_number, address_zipcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);")
+	storeAuthQuery := regexp.QuoteMeta("INSERT INTO auth (login, password) VALUES (?, ?);")
+
+	mock.ExpectBegin()
+	mock.ExpectPrepare(storeUserQuery)
+	mock.ExpectExec(storeUserQuery).WithArgs("", "", "", "", "", "", "", "", "", "").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectPrepare(storeAuthQuery)
+	mock.ExpectExec(storeAuthQuery).WithArgs("", "").WillReturnError(errors.New("error message"))
+	mock.ExpectRollback()
+
+	authMysqlRepository := NewAuthMysqlRepository(db)
+
+	err = authMysqlRepository.StoreWithUser(context.Background(), &domain.Auth{}, &domain.User{})
+
+	assert.Error(t, err)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestStoreWithUser(t *testing.T) {
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		t.Fatalf("error when opening a stub database conn %s", err)
+	}
+
+	storeUserQuery := regexp.QuoteMeta("INSERT INTO users (email, first_name, last_name, phone_number, address_city, address_state, address_neighborhood, address_street, address_number, address_zipcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);")
+	storeAuthQuery := regexp.QuoteMeta("INSERT INTO auth (login, password) VALUES (?, ?);")
+
+	mock.ExpectBegin()
+	mock.ExpectPrepare(storeUserQuery)
+	mock.ExpectExec(storeUserQuery).WithArgs("", "", "", "", "", "", "", "", "", "").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectPrepare(storeAuthQuery)
+	mock.ExpectExec(storeAuthQuery).WithArgs("", "").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	authMysqlRepository := NewAuthMysqlRepository(db)
+
+	err = authMysqlRepository.StoreWithUser(context.Background(), &domain.Auth{}, &domain.User{})
+
+	assert.NoError(t, err)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
 }
