cda3667eade850d4c2548f5e7b932bfda68a84b1
giovanisilqueirasantos <giovani.silqueira.santos@gmail.com>
2022-06-28 17:06:14 -0300

    add product domain structure

---
+++ domain/mocks/product.go
@@ -0,0 +1,20 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/giovanisilqueirasantos/e-commerce-go-clean-arch/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockProductUsecase struct {
+	mock.Mock
+}
+
+func (mpu *MockProductUsecase) Get(ctx context.Context, uuid string) (domain.Product, error) {
+	args := mpu.Called(ctx, uuid)
+	if args.Get(0) == nil {
+		return domain.Product{}, args.Error(1)
+	}
+	return domain.Product{ID: int64(args.Int(0)), UUID: args.String(1), Rate: float32(args.Int(2)), Pictures: []string{args.String(3)}, Name: args.String(4), Detail: args.String(5), Favorite: args.Bool(6), Attributes: []domain.Attribute{domain.Attribute{Label: args.String(7), Values: []string{args.String(8)}}}}, args.Error(9)
+}
---
+++ domain/product.go
@@ -0,0 +1,23 @@
+package domain
+
+import "context"
+
+type Attribute struct {
+	Label  string   `json:"label"`
+	Values []string `json:"values"`
+}
+
+type Product struct {
+	ID         int64
+	UUID       string      `json:"uuid"`
+	Rate       float32     `json:"rate"`
+	Pictures   []string    `json:"pictures"`
+	Name       string      `json:"name"`
+	Detail     string      `json:"detail"`
+	Favorite   bool        `json:"favorite"`
+	Attributes []Attribute `json:"attributes"`
+}
+
+type ProductUseCase interface {
+	Get(ctx context.Context, uuid string) (Product, error)
+}
